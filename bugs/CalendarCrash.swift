288571c802a402b8a7b9a7a589a09941e6dc11b1
David Jones <djones6@uk.ibm.com>
2017-01-31 10:50:00 +0000

    Add example of crash

--- bugs/CalendarCrash.swift
+++ bugs/CalendarCrash.swift
@@ -1,15 +1,14 @@
 import Foundation
 
 let formatter:DateFormatter = DateFormatter()
-
-let date:String = "Tue Jan 31 00:00:00 GMT 2017"
 formatter.dateFormat = "E MMM dd HH:mm:ss z yyyy"
-
+//formatter.calendar = Calendar(identifier: .gregorian)
 // This line causes a crash:
 formatter.calendar = Calendar(identifier: .iso8601)
 formatter.locale = Locale(identifier: "en_US_POSIX")
 formatter.timeZone = TimeZone(secondsFromGMT: 0)
 
+let date:String = "Tue Jan 31 00:00:00 GMT 2017"
 let result = formatter.date(from: date)
 if let result = result {
   print("Result: \(result)")
